a44e42551e9584e2ceb315e8a79aa223f88fad21
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-06-25 16:04:26 +0900

    객체 정리

--- javascript-practices/ex02.js
+++ javascript-practices/ex02.js
@@ -7,14 +7,24 @@
  * 3. string
  * 4. undefined
  *
- * [객체]
- * Number()
- * Boolean()
- * String()
-
+ * [객체1]- Object type
+ * 1. new 생성자함수()를 사용해서 생성
+ *   Number()
+ *   Boolean()
+ *   String()
+ *   Object()
+ * 2. null
+ * 3. [](배열 리터럴)
+ * 4. {}(객체 리터럴(json))
+ *
+ * [객체2]- function type
+ * function
  *
  */
-var u; // var u = undefined; 와 동일 자바 스크립트는 선언과 정의의 구분이 없다.
+
+console.log("[원시 타입]===================================");
+
+var u = undefined; // var u; 와 동일, 선언과 정의의 구분이 없다.
 var i = 10;
 var b = true;
 var s = "hello world";
@@ -24,4 +34,30 @@ console.log("i: " + typeof i); // i number
 console.log("b: " + typeof b); // b boolean
 console.log("s: " + typeof s); // s string
 
-console.log(s.toLocaleUpperCase()); // 잠깐 new String(s).to...로 실행됨
+console.log("[객체 1]===================================");
+
+var i2 = new Number(10);
+var b2 = new Boolean(true);
+var s2 = new String("Hello world");
+var o = new Object(true);
+var a = new Array();
+var n = null;
+var a2 = [];
+var o2 = {};
+
+console.log("i2: " + typeof i2); // i2: object
+console.log("b2: " + typeof b2); // b2: object
+console.log("s2: " + typeof s2); // s2: object
+console.log("o: " + typeof o); // o object
+console.log("a: " + typeof a); // a object
+console.log("n: " + typeof n); // n object
+console.log("o2: " + typeof o2); // o object
+console.log("a2: " + typeof a2); // a object
+
+console.log("[객체 2]===================================");
+
+var f = function (a, b) {
+  return a + b;
+};
+
+console.log("f: " + typeof f); // f function
