c7874d82a3ae218b6eba80ef596287e0f51318cb
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-07-06 14:54:40 +0900

    ajax #5

--- ch08/src/main/java/com/douzone/frontdev/ch08/controller/ApiController.java
+++ ch08/src/main/java/com/douzone/frontdev/ch08/controller/ApiController.java
@@ -1,7 +1,9 @@
 package com.douzone.frontdev.ch08.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.frontdev.ch08.dto.JsonResult;
@@ -35,15 +37,32 @@ public class ApiController {
 		return XmlResult.success(guestBookVo);
 	}
 	
+
 	@ResponseBody
-	@RequestMapping("/json")
-	public Object json() {
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public Object getJson() {
 		GuestbookVo guestBookVo = new GuestbookVo();
 		guestBookVo.setNo(1L);
 		guestBookVo.setName("둘리");
 		guestBookVo.setMessage("호이~~");
 		
 		return JsonResult.success(guestBookVo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post01", method = RequestMethod.POST)
+	public Object post01(GuestbookVo vo) {
+		vo.setNo(10L);
+		vo.setPassword("");
+		return JsonResult.success(vo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post02", method = RequestMethod.POST)
+	public Object post02(@RequestBody GuestbookVo vo) {
+		vo.setNo(10L);
+		vo.setPassword("");
+		return JsonResult.success(vo);
 	}
 	
 }
